93906cc6e1539ad4e813afeb153fa3ac9755474d
Amr Soliman <34183414+AmrASoliman@users.noreply.github.com>
2020-09-04 22:45:07 -0400

    image name changed

--- components/layout.js
+++ components/layout.js
@@ -28,7 +28,7 @@ export default function Layout({ children, home }) {
         {home ? (
           <>
             <img
-              src='/images/profile.jpg'
+              src='/images/Profile.jpg'
               className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
               alt={name}
             />
